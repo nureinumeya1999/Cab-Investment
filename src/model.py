c8875aa87e339fe54da71975c08bba12db15a873
Rein <nureinumarchesano@gmail.com>
2022-09-13 09:34:30 -0400

    US model'

--- src/model.py
+++ src/model.py
@@ -13,11 +13,7 @@ with open('queryData.json', 'r') as f:
 
 def train(X, y, path):
     regressor = LinearRegression()
-
-    #Fitting model with trainig data
     regressor.fit(X, y)
-
-    # Saving model to disk
     pickle.dump(regressor, open(os.path.join(path, 'model.pkl'),'wb'))
 
 
@@ -38,6 +34,20 @@ def generate_models():
                 train(np.reshape(X, (-1, 1)), y, MODEL_PATH)
 
 
+def generate_overalls():
+    for company in queryData['company']:
+        _company = queryData['company'][company]
+        for quarter in queryData['quarter']:
+            _quarter = queryData['quarter'][quarter]
+            MODEL_PATH = os.path.join('models', company, quarter, 'US')
+            query = f'Company == {_company} & Quarter == {_quarter}'
+        
+            data = dataset.query(query).groupby('Year', as_index=True).sum()
+            X = data.index
+            y = data['Profit']
+            train(np.reshape(X, (-1, 1)), y, MODEL_PATH)
+
+
 def predict(company, year, quarter, city):
 
     cwd = os.getcwd()
@@ -47,12 +57,5 @@ def predict(company, year, quarter, city):
     return model.predict([[year]])
 
 
-#generate_models()
 for result in predict('Yellow Cab', 2022, 4, 'NEW YORK NY'):
     print(result)
-
-        
-
-    # Loading model to compare the results
-
-    # print(model.predict([[2, 2200, 5]]))
